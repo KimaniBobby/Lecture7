c1fc76f126df5052a2258a043ed96e0817c54157
Bobby Kimani <bobbykimani@Bobbys-MacBook-Pro.local>
2021-10-17 15:55:21 +0300

    For it to work the value of b should be greater than that of a and c

---
+++ assignment2_1.py
@@ -0,0 +1,42 @@
+#assignment2_1.py (Created the first and second individually, hence the chang in naming convention
+
+#for there to be no error the value of b should be greater than that of a and c
+
+import math
+
+def introduction_message():
+    print("This program computes the roots")
+    print("of a second order equation:")
+    print()
+    print("     ax^2 + bx + c=0     ")
+    print()
+
+def input_and_solve():
+    a = eval(input("Please enter a: "))
+    b = eval(input("Please enter b: "))
+    c = eval(input("Please enter c: "))
+
+    delta = (b*b) - (4*a*c)
+    x1 = -b + math.sqrt(delta)
+    x2 = -b - math.sqrt(delta)
+
+    print()
+    print("The roots are :  ")
+    print("x1 =  ", x1)
+    print("x2 =  ", x2)
+
+def final_message():
+    print()
+    print(" Thank you for using this program")
+    print("=================================")
+    print("***   All rights reserved     ***")
+    print("=================================")
+    print()
+
+
+introduction_message()
+
+input_and_solve()
+
+final_message()
+    
