a0788e16e07dd458b06e68642c8982d3332391a0
Bobby Kimani <bobbykimani@Bobbys-MacBook-Pro.local>
2021-10-17 15:33:31 +0300

    assignment 1 in a function

---
+++ assignment1_2.py
@@ -0,0 +1,9 @@
+#assignment1_2.py
+
+def main():
+    print("Hello World")
+    print("Hello Again")
+    print('Yay!Printing.')
+    print("I'd much rather you 'not'.")
+    print('I"said"do not touch this.')
+main()
